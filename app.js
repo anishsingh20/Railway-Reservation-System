37f20c57614faebaa64527d6a42a7229d38e6dc8
Ansih Singh Walia <loveeanish@gmail.com>
2016-10-31 02:46:07 +0530

    Database Models Created for Train and Passanger

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongo = require('mongoose');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require(path.join(__dirname , '/routes'));
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -56,5 +56,8 @@ app.use(function(err, req, res, next) {
   });
 });
 
+app.listen(5555,function(){
+    console.log('App running Successfully, Enjoy!! ');
+});
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+//creating object for DB File in Model
+var Userdata = require('../model/Userdata');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/train');
+//name of the Database
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Railway reservation System' });
+
+  //if we don't want to use any view engine then we can simply use res.sendfile('PATH/FILENAME');
 });
 
+
+// exporting the routes file for use in other files
 module.exports = router;
